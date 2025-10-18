3436c05526d217d700ed57fb01a2d8ca9493b5f7
Alre2 <alaa.rekkab@lerniastudent.se>
2025-10-18 11:52:26 +0200

    script.js

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ function makeItem(text){
   const del=document.createElement("button");
   del.className="del";
   del.type="button";
-  del.innerHTML=`<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" d="M6 7h12M9 7v10m6-10v10M5 7l1 12a2 2 0 002 2h8a2 2 0 002-2l1-12M9 7V5a2 2 0 012-2h2a2 2 0 012 2v2"/></svg>`;
+  del.innerHTML='<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" d="M6 7h12M9 7v10m6-10v10M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2l1-12M9 7V5a2 2 0 0 1 2-2h2a2 2 0 0 1 2 2v2"/></svg>';
   li.appendChild(p);
   li.appendChild(del);
   return li;
